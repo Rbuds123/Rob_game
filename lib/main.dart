78cc6ee5654d53905bff7f638bab656ffd131154
Rbuds123 <rbuds@outlook.com>
2024-04-22 16:02:58 +0100

    adding firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:robs_game/authentication/register_page.dart';
+import 'firebase_options.dart';
 import 'main_page.dart';
 import 'main_game.dart';
 
-
-
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -13,7 +18,7 @@ Future<void> main() async {
       initialRoute: '/',
       routes: {
         //displays different screens
-        '/': (context) => const homePage(), // your main page
+        '/': (context) => RegistrationPage(), // your main page
         '/second': (context) => const myGame(), // your MyFlutterApp page);
       },
     ),
