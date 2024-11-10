abf3045aaddeba4620f592bd0608347d8e9029bd
Rbuds123 <108302846+Rbuds123@users.noreply.github.com>
2024-11-10 23:20:18 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,6 @@ import 'main_game.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
