f90649b8ad00c4c3cdd88d6a0cb5f3473497e150
Rbuds123 <108302846+Rbuds123@users.noreply.github.com>
2024-11-10 23:19:11 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:robs_game/authentication/register_page.dart';
-import 'firebase_options.dart';
 import 'main_page.dart';
 import 'main_game.dart';
 
